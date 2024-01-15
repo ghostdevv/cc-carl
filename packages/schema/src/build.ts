f376d49be0c39f01083880f7f871c54766e2d444
GHOST <ghostdevbusiness@gmail.com>
2024-01-15 20:19:38 +0000

    fix: add $schema to schema
    Co-authored-by: NimajnebEC <NimajnebEC@users.noreply.github.com>

--- packages/schema/src/build.ts
+++ packages/schema/src/build.ts
@@ -1,20 +1,22 @@
 import { repositorySchema, packageSchema } from './index';
 import { zodToJsonSchema } from 'zod-to-json-schema';
 import { writeFile, mkdir } from 'node:fs/promises';
+import { type AnyZodObject, z } from 'zod';
 import { join } from 'desm';
 
-const SCHEMA_DIR = join(import.meta.url, '../schema');
+const SCHEMA_DIR = join(import.meta.url, '../json-schema');
 
 await mkdir(SCHEMA_DIR, { recursive: true });
 
-await writeFile(
-	`${SCHEMA_DIR}/repositorySchema.json`,
-	JSON.stringify(zodToJsonSchema(repositorySchema), null, 2),
-	'utf-8',
-);
+async function writeSchema(name: string, zodSchema: AnyZodObject) {
+	const schema = zodSchema.extend({ $schema: z.string() });
 
-await writeFile(
-	`${SCHEMA_DIR}/packageSchema.json`,
-	JSON.stringify(zodToJsonSchema(packageSchema), null, 2),
-	'utf-8',
-);
+	await writeFile(
+		`${SCHEMA_DIR}/${name}.json`,
+		JSON.stringify(zodToJsonSchema(schema), null, 2),
+		'utf-8',
+	);
+}
+
+await writeSchema('repository', repositorySchema);
+await writeSchema('package', packageSchema);
